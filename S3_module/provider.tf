8fa4073d637533b77589d9a816faea98b9c0afdd
Stanley Ibe <stancima@yahoo.com>
2024-04-11 17:14:39 -0500

    testing vscode/git integration

--- S3_module/provider.tf
+++ S3_module/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = var.aws_region # Adjust the region as needed
+  region = var.aws_region # Adjust the aws region here as needed
 
   access_key = var.aws_access
   secret_key = var.aws_secret
